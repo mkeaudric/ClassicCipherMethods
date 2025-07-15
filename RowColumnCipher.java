23a0599188a3cbb705cd2f3b754831b1591e2bfd
Michael Audric <mikeaudric123@gmail.com>
2025-07-15 18:04:55 +0700

    added RowColumnCipher.java

---
+++ RowColumnCipher.java
@@ -0,0 +1,150 @@
+package CipherClassic;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
+public class RowColumnCipher {
+    public static String initInput(String input){
+        StringBuilder sb = new StringBuilder();
+        int inputLen = input.length();
+        char c;
+
+        for(int i = 0; i < inputLen; i++){
+            c = input.charAt(i);
+            if(c == ' ')
+                continue;  
+            else
+                sb.append(Character.toUpperCase(c));
+        }
+
+        return sb.toString();
+    }
+
+    public static String generateKey(int n){
+        // trik : Collections.shuffle
+        List<Integer> nums = new ArrayList<>();
+        for(int i = 1; i <= n; i++) nums.add(i);
+        Collections.shuffle(nums);
+
+        StringBuilder sb = new StringBuilder();
+        for(int num : nums){
+            sb.append(num);
+        }
+        return sb.toString();
+    }
+    
+    private static String encodeRowColumn(String input, String key, int N, int M) {
+        int i, j, inputLen = input.length();
+        char[][] array = new char[N][M];
+
+        int ct = 0;
+        for(i = 0; i < N; i++){
+            for(j = 0; j < M; j++){
+                if(ct < inputLen){
+                    array[i][j] = input.charAt(ct);
+                    ct++;
+                } else{
+                    array[i][j] = ((char)((Math.random()*100) %26 + 'A'));
+                }
+            }
+        }
+
+        for(i = 0; i < N; i++){
+            for(j = 0; j < M; j++){
+                System.out.print(array[i][j] + " ");
+            } System.out.println();
+        }
+
+        StringBuilder sb = new StringBuilder();
+        // gua yakin bagian ini bisa di optimisasi, mungkin di input,
+        // tapi utk sekarang masih pakai double loop utk ngambil sesuai urutan
+        int k, keyLen = key.length();
+        for(j = 0; j < keyLen; j++){
+            for(k = 0; k < keyLen; k++){
+                if(j == key.charAt(k) - '1'){ // -'1' karena key dari 1, sementara maunya dari 0
+                    for(i = 0; i < N; i++){
+                        sb.append(array[i][k]);
+                    } 
+                }
+            }
+        }
+        return sb.toString();
+    }
+
+    private static String decodeRowColumn(String input, String key, int N, int M) {
+        int i, j;
+        char[][] array = new char[N][M];
+
+        // key.length() = M, jadi pake M aja
+        int ct = 0;
+        for(j = 0; j < M; j++){
+            for(i = 0; i < N; i++){
+                array[i][j] = input.charAt(ct++);
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for(i = 0; i < N; i++){
+            for(j = 0; j < M; j++){
+                sb.append(array[i][key.charAt(j) - '1']);
+            }
+        }
+        return sb.toString();
+    }
+
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Masukan plaintext : ");
+        String input = sc.nextLine();
+        input = initInput(input);
+
+        int inputLen = input.length();
+
+        int N, M;
+
+        while(true){
+            System.out.println("Masukan ukuran matriks NxM");
+            System.out.print("N : ");
+            N = sc.nextInt();
+            System.out.print("M : ");
+            M = sc.nextInt();
+            if(N*M >= inputLen) break;
+            else System.out.println("NxM harus lebih besar/sama dengan dari panjang plaintext!");
+        }
+
+        // String key = generateKey(M);
+        String key = "4312567";
+        System.out.println("Generated key : " + key);
+
+        sc.nextLine();
+
+        System.out.println("1. Encode");
+        System.out.println("2. Decode");
+        int action;
+        while(true){
+            String actionString = sc.nextLine();
+
+            try {
+                action = Integer.parseInt(actionString);
+                if(action == 1 || action == 2){
+                    break;
+                } else{
+                    System.out.println("Aksi yang bisa dipilih hanya 1 atau 2");
+                }
+            } catch (NumberFormatException e){
+                System.out.println("Hanya bisa masukan angka");
+            }
+        }
+
+        if(action == 1){ // encode
+            System.out.println(encodeRowColumn(input, key, N, M));
+        } else { // decode
+            System.out.println(decodeRowColumn(input, key, N, M));
+        }
+
+        sc.close();
+    }
+}
